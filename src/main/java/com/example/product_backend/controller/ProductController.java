1f14ef3db63b472e5deabd4a88e57980d8a4363d
akhil <akhil.paul@ymail.com>
2022-12-05 19:48:31 +0530

    Add product page routing

--- src/main/java/com/example/product_backend/controller/ProductController.java
+++ src/main/java/com/example/product_backend/controller/ProductController.java
@@ -1,5 +1,12 @@
 package com.example.product_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class ProductController {
+    @PostMapping
+    public String addProductPage(){return "Add product page";}
+
+
 
 }
